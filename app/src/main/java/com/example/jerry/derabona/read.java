af3674c0818bf19e605c9974a63b8aadc5e2632c
Jerry <mrjosorio@gmail.com>
2017-07-15 20:07:49 -0400

    Updates to read class and adding rows for contents of each match

--- app/src/main/java/com/example/jerry/derabona/read.java
+++ app/src/main/java/com/example/jerry/derabona/read.java
@@ -1,39 +1,84 @@
 package com.example.jerry.derabona;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class read extends AppCompatActivity {
 
-    private TextView txt_name;
     private DatabaseReference firebaseDatabase;
+
+    private EditText edt_input_name;
+    private EditText edt_input_password;
+
+    private TextView txt_data;
+
+    private Button btn_add;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_read);
+        setContentView(R.layout.activity_bets);
 
-        txt_name = (TextView) findViewById(R.id.ar_txt_name);
-        firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("matches");
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
 
-        firebaseDatabase.addValueEventListener(new ValueEventListener() {
+        txt_data = (TextView) findViewById(R.id.ab_txt_data);
+
+
+        DatabaseReference example = firebaseDatabase.child("matches");
+
+
+        ValueEventListener eventListener = new ValueEventListener() {
+
+            match match = new match();
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                txt_name.setText(dataSnapshot.toString().trim());
+                ArrayList<match> matches = new ArrayList<>();
+
+                for(int i = 1; i <= dataSnapshot.getChildrenCount(); i++){
+                    matches.add(dataSnapshot.child(i+"").getValue(match.getClass()));
+                    Log.i("test", matches.get(i-1).getMatch());
+
+
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Toast.makeText(read.this, "Error: ", Toast.LENGTH_LONG).show();
             }
-        });
+        };
+        example.addValueEventListener(eventListener);
 
+    }
+
+    public String rowToString(match match){
+        String data = "";
+        data += match.getDate().toString() +"     "+
+                match.getMatch() +"     "+
+                match.getStatus()+"     "+
+                match.getWinner();
+
+        return data;
     }
 }
