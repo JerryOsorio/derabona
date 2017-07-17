94238ecd0854f0afeae11636bb8ef156808dfb1a
Jerry <mrjosorio@gmail.com>
2017-07-17 16:08:41 -0400

    Implementation of barebones read data functionality. Default screen made for matches in progress and closed matches

--- app/src/main/java/com/example/jerry/derabona/read_adapter.java
+++ app/src/main/java/com/example/jerry/derabona/read_adapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -35,13 +36,24 @@ public class read_adapter extends ArrayAdapter<match>{
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View view = inflater.inflate(R.layout.activity_read_row, parent, false);
 
+        if(!getItem(position).getStatus().equals("open")){
+
+            LinearLayout ll_bottom = view.findViewById(R.id.ar_ll_bottom);
+            ll_bottom.setVisibility(View.GONE);
+
+            TextView txt_progress = view.findViewById(R.id.ar_txt_progress);
+            txt_progress.setVisibility(View.VISIBLE);
+            txt_progress.setText(getItem(position).getStatus());
+
+        }
+
+
         match match  = getItem(position);
         String description = match.getMatch();
         String date = match.getDate();
 
         TextView txt_date = (TextView) view.findViewById(R.id.ab_txt_date);
         TextView txt_description = (TextView) view.findViewById(R.id.ab_txt_match);
-
 
         Log.i("test", date +"   found");
         Log.i("test", description +"   found");
